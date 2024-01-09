255b4fa9910e7e29d32b70f282a3e1a8cd2af000
adomavp2 <paulina.adomaviciute2@mail.dcu.ie>
2024-01-09 09:15:41 +0000

    Layout, add comments, change some variable names to reflect their purpose better, move API call function outside of main().

--- code/callAPI.java
+++ code/callAPI.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.IOException;
 
+//HTTP request libraries
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -9,30 +10,42 @@ import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
+
+//JSON libraries
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 class callAPI {
-private static final String IMAGE1 = "img/1.jpg";
-private static final String IMAGE2 = "img/2.jpg";
+    //Images to be given to API. Mind the naming.
+    private static final String IMAGE1 = "img/1.jpg";
+    private static final String IMAGE2 = "img/2.jpg";
 
-private static final String PROJECT = "all";
-private static final String URL = "https://my-api.plantnet.org/v2/identify/" + PROJECT + "?api-key=2b10LhD3grOwCbkvSKtnGw58s";
+    //API call parameters.
+    private static final String PROJECT = "all";
+    private static final String URL = "https://my-api.plantnet.org/v2/identify/" + PROJECT + "?api-key=2b10LhD3grOwCbkvSKtnGw58s";
 
-    public static void main(String[] args) {
-        File file1 = new File(IMAGE1);
-        File file2 = new File(IMAGE2);
-        
+    //Function to find scientific name of the plant species based on images submitted. Returns latin name of the plant species.
+    static String findScientificName() {
+
+        //Set variable for scientific name as N/A in case plant recognition fails.
+        String scientificName = "N/A";
+
+        File image1 = new File(IMAGE1);
+        File image2 = new File(IMAGE2);
+
+        //Create HTTPEntity for API request.
         HttpEntity entity = MultipartEntityBuilder.create()
-            .addPart("images", new FileBody(file1)).addTextBody("organs", "auto")
-            .addPart("images", new FileBody(file2)).addTextBody("organs", "auto")
+            .addPart("images", new FileBody(image1)).addTextBody("organs", "auto")
+            .addPart("images", new FileBody(image2)).addTextBody("organs", "auto")
             .build();
         
+        //Prepare to POST HTTP request.
         HttpPost request = new HttpPost(URL);
         request.setEntity(entity);
-        
         HttpClient client = HttpClientBuilder.create().build();
         HttpResponse response;
+
+        //Try to get a response from API.
         try {
             response = client.execute(request);
             String jsonString = EntityUtils.toString(response.getEntity());
@@ -40,10 +53,14 @@ private static final String URL = "https://my-api.plantnet.org/v2/identify/" + P
             JSONArray results = jsonObj.getJSONArray("results");
             JSONObject firstResult = results.getJSONObject(0);
             JSONObject species = firstResult.getJSONObject("species");
-            String scientificName = species.getString("scientificNameWithoutAuthor");
-            System.out.println(scientificName);
-        } catch (IOException e) {
+            scientificName = species.getString("scientificNameWithoutAuthor");
+        } catch (IOException e) { //In case something goes wrong. TODO: better exception handling?
             e.printStackTrace();
         }
+
+        return scientificName;
+    }
+    public static void main(String[] args) {
+        System.out.println(findScientificName());
     }
 }
