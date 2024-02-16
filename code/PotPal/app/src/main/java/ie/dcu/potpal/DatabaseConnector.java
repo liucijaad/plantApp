72e848cbf1b3c80817d30e4fb63d85a932a06e9a
adomavp2 <paulina.adomaviciute2@mail.dcu.ie>
2024-02-16 15:34:34 +0000

    DriverConnector: finish code for connecting to DB and getting plant data from there.

--- code/PotPal/app/src/main/java/ie/dcu/potpal/DatabaseConnector.java
+++ code/PotPal/app/src/main/java/ie/dcu/potpal/DatabaseConnector.java
@@ -7,19 +7,19 @@ import java.util.List;
 import java.sql.*;
 
 public class DatabaseConnector {
-    static final String connectionURL = "jdbc:sqlite:/sql/plant_data";
+    static final String connectionURL = "jdbc:sqlite:ie/dcu/potpal/sql/plant_data";
     public static List<String> connectToDatabase(String plantLatinName) {
         List<String> plantData = new ArrayList<>();
-        String query = "SELECT * FROM plant_data WHERE `Latin Name` == " + "'" + plantLatinName + "'";
+        String query = "SELECT x.* FROM \"Plant Data\" x WHERE `Latin Name` == " + "'" + plantLatinName + "'";
         try(Connection conn = DriverManager.getConnection(connectionURL);
             Statement statement = conn.createStatement();
             ResultSet result = statement.executeQuery(query);) {
-            for(int i=0; i<5; i++) {
+            for(int i=1; i<=5; i++) {
                 plantData.add(result.getString(i));
             }
         }
         catch (SQLException e) {
-            Log.e("DatabaseConnector", "Unable to connect to database. Error code: " + e.getErrorCode() + " " + e.getMessage());
+            Log.e("DatabaseConnector", "Unable to connect to database. Error code: " + e.getErrorCode() + " Error: " + e.getMessage());
         }
         return plantData;
     }
