be723f5baab9003e34d6eb72c7d52056a1fd431c
adomavp2 <paulina.adomaviciute2@mail.dcu.ie>
2024-02-19 13:17:53 +0000

    PlantIdentify code clean up. Added comments. DatabseConnector rename connectToDatabase to fetchFromDatabase to clarify method function. Add insertToDatabase method. Add commeents. Clean up code.

--- code/PotPal/app/src/main/java/ie/dcu/potpal/DatabaseConnector.java
+++ code/PotPal/app/src/main/java/ie/dcu/potpal/DatabaseConnector.java
@@ -7,10 +7,20 @@ import java.util.List;
 import java.sql.*;
 
 public class DatabaseConnector {
+    /**connectionURL Static URL address of the database.*/
     static final String connectionURL = "jdbc:sqlite:ie/dcu/potpal/sql/plant_data";
-    public static List<String> connectToDatabase(String plantLatinName) {
+
+    /**
+     * Fetches data of specified plant from database using plant's latin name.
+     * @param plantLatinName Plant's latin name.
+     * @return List of string with plant's latin name, type, sunlight needs, water needs and common names.
+     */
+    public static List<String> fetchFromDatabase(String plantLatinName) {
+        //List to store fetched data about plant
         List<String> plantData = new ArrayList<>();
+        //SQL query to select plant data.
         String query = "SELECT x.* FROM \"Plant Data\" x WHERE `Latin Name` == " + "'" + plantLatinName + "'";
+        //Attempt to connect to database and execute statement.
         try(Connection conn = DriverManager.getConnection(connectionURL);
             Statement statement = conn.createStatement();
             ResultSet result = statement.executeQuery(query);) {
@@ -19,8 +29,29 @@ public class DatabaseConnector {
             }
         }
         catch (SQLException e) {
-            Log.e("DatabaseConnector", "Unable to connect to database. Error code: " + e.getErrorCode() + " Error: " + e.getMessage());
+            Log.e("DatabaseConnectorFetch", "Unable to connect to database. Error code: " + e.getErrorCode() + " Error: " + e.getMessage());
         }
         return plantData;
+    }
+
+    /**
+     * Inserts plant data into the database.
+     * @param latinName Plant's latin name. Primary key. Cannot be duplicate in the database.
+     * @param type String of plant's type (ex. Succulent, Cactus, Vine).
+     * @param sunlight String of plant's sunlight needs (ex. Full Sunlight, Partial Shade).
+     * @param water String of plant's water needs (ex. Mesic, Dry Mesic).
+     * @param commonNames String of other common names plant is known by. If unknown, should be N/A.
+     */
+    public static void insertToDatabase(String latinName, String type, String sunlight, String water, String commonNames) {
+        String query = "INSERT INTO \"Plant Data\" VALUES ('" + latinName + "', '" + type + "', '" + sunlight + "', '" + water + "', '" + commonNames + "')";
+        try{
+            Connection conn = DriverManager.getConnection(connectionURL);
+            Statement statement = conn.createStatement();
+            statement.executeUpdate(query);
+            conn.close();
+        }
+        catch (SQLException e) {
+            Log.e( "DatabseConnectorInsert", "Unable to connect to dabase. Error code: " + e.getErrorCode() + " " + e.getMessage());
+        }
     }
 }
